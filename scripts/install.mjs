a2ad4062be892625c86885a9a2022e4f929aef8a
1chiSensei <mail@tomio.codes>
2021-11-29 18:37:24 +0800

    0.0.10

--- scripts/install.mjs
+++ scripts/install.mjs
@@ -1,10 +1,10 @@
-import { writeFile } from 'node:fs/promises';
-import { blue, red, green } from 'colorette';
+import { writeFile } from 'node:fs/promises'
 import { createRequire } from 'node:module';
 import { platform as getPlatform, arch as getArch } from 'node:os';
 import { platformArchTriples } from '@napi-rs/triples';
-import petitio from 'petitio';
 import { exit } from 'node:process';
+import needle from 'needle';
+import { createWriteStream } from 'node:fs';
 
 const r = createRequire(import.meta.url);
 const { version, name } = r('../package.json');
@@ -13,14 +13,4 @@ const arch = getArch();
 const { platformArchABI } = platformArchTriples[platform][arch][0];
 const URL = `https://github.com/1chiSensei/evaluate-math/releases/download/${version}/${name}.${platformArchABI}.node`;
 
-console.log(blue('Downloading native binding...'));
-
-const { body, statusCode } = await petitio(URL, 'GET').send();
-
-if (statusCode >= 400) {
-	console.log(red(`An error occured while downloading. Please try again. Status Code: ${statusCode}`));
-	exit(1);
-}
-
-await writeFile('evaluate-math.node', body);
-console.log(green('Download complete!'));
+needle.get(URL).pipe(createWriteStream('evaluate-math.node'))
