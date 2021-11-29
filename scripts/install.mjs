923fdafb4d2a92be9e4346bc94f871fb6bb6c991
1chiSensei <mail@tomio.codes>
2021-11-29 19:22:08 +0800

    0.0.11

--- scripts/install.mjs
+++ scripts/install.mjs
@@ -1,16 +1,20 @@
-import { writeFile } from 'node:fs/promises'
 import { createRequire } from 'node:module';
 import { platform as getPlatform, arch as getArch } from 'node:os';
 import { platformArchTriples } from '@napi-rs/triples';
-import { exit } from 'node:process';
-import needle from 'needle';
 import { createWriteStream } from 'node:fs';
+import { promisify } from 'node:util';
+import { pipeline as pipelineSync } from 'node:stream';
+import fetch from 'node-fetch';
 
+const pipeline = promisify(pipelineSync);
 const r = createRequire(import.meta.url);
 const { version, name } = r('../package.json');
 const platform = getPlatform();
 const arch = getArch();
 const { platformArchABI } = platformArchTriples[platform][arch][0];
 const URL = `https://github.com/1chiSensei/evaluate-math/releases/download/${version}/${name}.${platformArchABI}.node`;
+const { body, ok, status, statusText } = await fetch(URL);
 
-needle.get(URL).pipe(createWriteStream('evaluate-math.node'))
+if (!ok) throw new Error(`An error occured: ${status} ${statusText}`);
+
+await pipeline(body, createWriteStream('./evaluate-math.node'));
