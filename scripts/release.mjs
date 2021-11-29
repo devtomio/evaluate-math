81528be906727b4d444e59a5f6236f4609a48e97
1chiSensei <mail@tomio.codes>
2021-11-29 14:46:00 +0800

    0.0.1

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -2,6 +2,8 @@ import { execSync } from 'node:child_process';
 import { Octokit } from '@octokit/rest';
 import publish from '@jsdevtools/npm-publish';
 import { createRequire } from 'node:module';
+import { readdir, stat } from 'node:fs/promises';
+import { createReadStream } from 'node:fs';
 
 const r = createRequire(import.meta.url);
 const { version } = r('../package.json');
@@ -10,13 +12,32 @@ const log = execSync('git log -1 --pretty=%B', { encoding: 'utf8' }).trim();
 const gh = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
 if (re.test(log)) {
-	await gh.repos.createRelease({
+	const release = await gh.repos.createRelease({
 		owner: '1chiSensei',
 		repo: 'evaluate-math',
 		tag_name: version
 	});
 	await publish({
-		package: '../package.json',
 		token: process.env.NPM_TOKEN
 	});
+
+	const files = (await readdir('.')).filter((v) => v.endsWith('.node'));
+
+	for (const file of files) {
+		const { size } = await stat(`./${file}`);
+		await gh.repos.uploadReleaseAsset({
+			owner: '1chiSensei',
+			repo: 'evaluate-math',
+			name: file,
+			release_id: release.data.id,
+			mediaType: {
+				format: 'raw'
+			},
+			headers: {
+				'Content-Length': size,
+				'Content-Type': 'application/octet-stream'
+			},
+			data: createReadStream(`./${file}`)
+		});
+	}
 }
